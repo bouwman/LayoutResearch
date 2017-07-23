c707bd2fc37d67ea2055cacad939bcf6d011e63e
Tassilo Bouwman <tassilo@Tassilos-MacBook-Pro.local>
2017-07-23 19:25:36 +0100

    first test

--- LayoutResearch/ViewController.swift
+++ LayoutResearch/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import ResearchKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let step = OR
-        let task = ORKOrderedTask(identifier: "task", steps: [myStep])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        let introStep = ORKInstructionStep(identifier: "intro")
+        
+        introStep.title = "Welcome to ResearchKit"
+        
+        let searchStep = SearchStep(identifier: "asdf")
+        
+        let task = ORKOrderedTask(identifier: "task", steps: [introStep, searchStep])
+        
+        
+        let taskViewController = ORKTaskViewController(task: task, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,3 +41,25 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: ORKTaskViewControllerDelegate {
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        
+        let taskResult = taskViewController.result
+        // You could do something with the result here.
+        
+        // Then, dismiss the task view controller.
+        dismiss(animated: true, completion: nil)
+    }
+}
+class SearchStep: ORKStep {
+    override init(identifier: String) {
+        super.init(identifier: identifier)
+        
+        title = "Search"
+        text = "Search this item"
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
