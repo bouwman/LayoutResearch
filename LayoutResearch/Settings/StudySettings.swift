0f9f4c971b9f8b7ba136dbce853290169ae46028
Tassilo Bouwman <t.bouwman@me.com>
2017-08-14 20:44:08 +0100

    fix settings

--- LayoutResearch/Settings/StudySettings.swift
+++ LayoutResearch/Settings/StudySettings.swift
@@ -54,7 +54,7 @@ enum ParticipantGroup: String, CustomStringConvertible, SelectionPresentable {
         }
     }
     
-    private static var allGroups: [ParticipantGroup] {
+    static var allGroups: [ParticipantGroup] {
         return [.a,.b,.c,.d,.e,.f,.g,.h,.i,.j,.k,.l,.m,.n,.o,.p,.q,.r,.s,.t,.u,.v,.w,.x]
     }
     
